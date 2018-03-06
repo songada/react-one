b01f2c1728bdf38857ee1842bc88a80c211dfdb4
Song Li <songli@versa-networks.com>
2018-03-06 14:15:10 -0800

    master one

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React Change Six
+          <h1 className="App-title">Welcome to React Change master one
           </h1>
         </header>
         <p className="App-intro">
