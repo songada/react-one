bf94deda1c08639824edb7858f93bcdccf8a47cb
Song Li <songli@versa-networks.com>
2018-03-06 11:49:34 -0800

    master yi

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React Change Four
+          <h1 className="App-title">Welcome to React Change in master yi
           </h1>
         </header>
         <p className="App-intro">
